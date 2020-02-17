f4a231d7041eaa86ef595d60c0cea5f01f63d486
anormanwtc <anorman@student.wethinkcode.co.za>
2020-02-17 14:28:46 +0200

    get;set; added

--- Models/TaskModel.cs
+++ Models/TaskModel.cs
@@ -3,14 +3,19 @@ namespace Website.Models
 {
     public class TaskModel
     {
+        [Key]
         public int Id { get; set; }
-        public string TaskName
-        public int ProjectId
-        public int UserId
-        public string TaskDescription
-        public int StartDate
-        public int DueDate
+        [Required]
+        public string TaskName { get; set; }
+        [Required]
+        public int ProjectId { get; set; }
+        [Required]
+        public int UserId { get; set; }
+        [Required]
+        public string TaskDescription { get; set; }
+        public int StartDate { get; set; }
+        public int DueDate { get; set; }
         public int Flags { get; set; } //0-100 or -2 = stuck etc?
-        public string Comments
+        public string Comments { get; set; }
     }
 }
