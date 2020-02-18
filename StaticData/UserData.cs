84d69d2cc7a85d1eaea254bc67f0569851a504be
anormanwtc <anorman@student.wethinkcode.co.za>
2020-02-18 13:13:42 +0200

    Userdata id -> UserId & ProjModel int -> DateTime

--- StaticData/UserData.cs
+++ StaticData/UserData.cs
@@ -14,9 +14,9 @@ namespace Website.StaticData
 
         private static List<UserModel> listOfUsers = new List<UserModel>()
         {
-            new UserModel() { Id = 1, Email = "email@what.creativity", Password = "needsHash", FullName = "Tom Scott", Role = true },
-            new UserModel() { Id = 2, Email = "email@example", Password = "defaultsUnhashed", FullName = "Minion", Role = false },
-            new UserModel() { Id = 3, Email = "email@test.default", Password = "mustHash", FullName = "Slave", Role = false },
+            new UserModel() { UserId = 1, Email = "email@what.creativity", Password = "needsHash", FullName = "Tom Scott", Role = true },
+            new UserModel() { UserId = 2, Email = "email@example", Password = "defaultsUnhashed", FullName = "Minion", Role = false },
+            new UserModel() { UserId = 3, Email = "email@test.default", Password = "mustHash", FullName = "Slave", Role = false },
         };
     }
 }
