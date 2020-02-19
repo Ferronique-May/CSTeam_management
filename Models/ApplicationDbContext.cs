c12185f5711f6b49ae80e8642f622631d45d13a3
anormanwtc <anorman@student.wethinkcode.co.za>
2020-02-19 07:41:02 +0200

    removed practice/testing Person set

--- Models/ApplicationDbContext.cs
+++ Models/ApplicationDbContext.cs
@@ -8,7 +8,6 @@ namespace Website.Models
             : base(options)
         {
         }
-        public DbSet<Person> People { get; set; }
         public DbSet<UserModel> Users { get; set; }
         public DbSet<TaskModel> Tasks { get; set; }
         public DbSet<ProjectModel> Projects { get; set; }
